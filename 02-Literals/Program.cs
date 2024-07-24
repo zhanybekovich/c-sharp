260fb4e33dd75a40143d9fb5224f28261c9da63e
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-07-24 21:34:40 +0600

    Literals

---
+++ 02-Literals/Program.cs
@@ -0,0 +1,52 @@
+﻿// Literals
+
+// Logical literals
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+// Int
+Console.WriteLine(-11);
+Console.WriteLine(5);
+Console.WriteLine(505);
+
+// Bin nums
+Console.WriteLine(0b11); // 3
+Console.WriteLine(0b1011); // 11
+Console.WriteLine(0b100001); // 33
+
+// Hex nums
+Console.WriteLine(0x0A);    // 10
+Console.WriteLine(0xFF);    // 255
+Console.WriteLine(0xA1);    // 161
+
+// Floats
+Console.WriteLine(3.14);   
+Console.WriteLine(100.001);   
+Console.WriteLine(-0.38);
+
+Console.WriteLine(3.2e3);
+Console.WriteLine(100.001);
+
+// Symbol
+Console.WriteLine('2');
+Console.WriteLine('A');
+Console.WriteLine('T');
+
+// ASCII
+Console.WriteLine('\x78');    // x
+Console.WriteLine('\x5A');    // Z
+
+// UNICODE
+Console.WriteLine('\u0420');    // Р
+Console.WriteLine('\u0421');    // С
+
+// Escape Sequences
+Console.WriteLine('\n');
+Console.WriteLine('\t');
+Console.WriteLine('\\');
+
+// String Literals
+Console.WriteLine("hello");
+Console.WriteLine("фыва");
+Console.WriteLine("hello word");
+
