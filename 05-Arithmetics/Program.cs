d4fcde918c0a9d6d04b673de41d1dcdaddd30c16
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-07-25 11:35:10 +0600

    Arithmetics

---
+++ 05-Arithmetics/Program.cs
@@ -0,0 +1,33 @@
+﻿// +
+int x = 10;
+int z = x + 12;
+
+// -
+x = 10;
+z = x - 6;
+
+// *
+x = 10;
+z = x * 6;
+
+// "/" - division
+x = 10;
+z = x / 5;
+
+// module
+x = 10;
+z = x % 5;
+
+// increment
+int x1 = 5;
+Console.WriteLine(++x1);
+
+int x2 = 5;
+Console.WriteLine(x2++);
+
+// decrement
+int x3 = 5;
+Console.WriteLine(--x1);
+
+int x4 = 5;
+Console.WriteLine(x2--);
