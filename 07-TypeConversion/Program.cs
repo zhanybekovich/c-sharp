9cfa661600f53200144ba8a68de0e5e0d74bf4d8
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-07-25 12:22:46 +0600

    Type conversion

---
+++ 07-TypeConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿// Types that can be savely automatically converted
+/*
+ * byte => short, ushort, int, uint, long, ulong, float, double, decimal
+ * sbyte => short, int, long, float, double, decimal
+ * short => int, long, float, double, decimal
+ * ushort => int, uint, long, ulong, float, double, decimal
+ * int => long, float, double, decimal
+ * uint => long, ulong, float, double, decimal
+ * long => float, double, decimal
+ * ulong => float, double, decimal
+ * float => double
+ * char => ushort, int, uint, long, ulong, float, double, decimal
+ * In other situations use EXPLICIT conversion
+ */
+
+// Explicit conversion
+double a = 4.0;
+decimal b = (decimal)a;
+Console.WriteLine(b);
