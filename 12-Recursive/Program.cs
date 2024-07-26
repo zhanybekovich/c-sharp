808b0a2ff52dd854d9b19249f21cf4d19cc2edc4
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-07-26 17:01:06 +0600

    Recursive functions

---
+++ 12-Recursive/Program.cs
@@ -0,0 +1,21 @@
+﻿// factorial
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+
+    return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(4));
+Console.WriteLine(Factorial(5));
+
+// fibonachi
+int Fibonachi(int n)
+{
+    if (n == 0 || n == 1) return n;
+
+    return Fibonachi(n-1) + Fibonachi(n-2);
+}
+
+Console.WriteLine(Fibonachi(4));
+Console.WriteLine(Fibonachi(5));
