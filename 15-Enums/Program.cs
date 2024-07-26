f74ec043ab955b5b914b7c2db8d3e36afc729d9d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-07-26 17:48:21 +0600

    Enums

---
+++ 15-Enums/Program.cs
@@ -0,0 +1,52 @@
+﻿DayTime now = DayTime.Evening;
+
+PrintMessage(now);                  
+PrintMessage(DayTime.Afternoon);    
+PrintMessage(DayTime.Night);        
+
+void PrintMessage(DayTime dayTime)
+{
+    switch (dayTime)
+    {
+        case DayTime.Morning:
+            Console.WriteLine("Доброе утро");
+            break;
+        case DayTime.Afternoon:
+            Console.WriteLine("Добрый день");
+            break;
+        case DayTime.Evening:
+            Console.WriteLine("Добрый вечер");
+            break;
+        case DayTime.Night:
+            Console.WriteLine("Доброй ночи");
+            break;
+    }
+}
+
+enum DayTime
+{
+    Morning,
+    Afternoon,
+    Evening,
+    Night
+}
+
+// enum types
+/* byte, sbyte, short, ushort, int, uint, long, ulong */
+enum Time : byte
+{
+    Morning,
+    Afternoon,
+    Evening,
+    Night
+}
+
+// explicit values
+enum DayTime2
+{
+    Morning = 2,
+    Afternoon = 4,
+    Evening = 8,
+    Night = 16
+}
+
