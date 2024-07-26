d06ffc57d0bddba78501ac1b068c10422d36ee97
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-07-26 17:59:29 +0600

    Basic class

---
+++ 16-Class/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Person tom = new Person();
+Console.WriteLine(tom.name);
+Console.WriteLine(tom.age);
+
+tom.name = "Tom";
+tom.age = 10;
+
+tom.Print();
+class Person
+{
+    public string name = "Undefined";
+    public int age;
+
+public void Print()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
+
