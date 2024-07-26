7a60828642bc6bd0b3966d8a4bf73ffd57472e35
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-07-26 20:19:03 +0600

    Structures

---
+++ 18-Structures/Program.cs
@@ -0,0 +1,24 @@
+﻿Person tom = new Person("Tom", 10);
+tom.Print();
+
+// copy structure
+Person bob = tom with { name = "Bob" };
+bob.Print();
+
+struct Person
+{
+    public string name;
+    public int age;
+
+    // constructor
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
